127fdb500fd433894a3fbdeb0db7058935e9a66c
matttodd <matttodd6@gmail.com>
2018-12-26 21:27:37 -0500

    secrets

--- venv/src/__init__.py
+++ venv/src/__init__.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return "Hello World"
+   return "Welcome to Flask"
+
+@app.route('/secret')
+def hello_secret():
+   return "Welcome to (secret) Flask"
 
 if __name__ == '__main__':
    app.run()
