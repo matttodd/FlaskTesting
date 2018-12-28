9d41fae6fa2af7bc2ac39e99ab152d42b5b8669b
matttodd <matttodd6@gmail.com>
2018-12-28 16:20:57 -0500

    good lord

--- venv/src/__init__.py
+++ venv/src/__init__.py
@@ -1,24 +1,18 @@
-from flask import Flask,redirect, url_for
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return "Welcome to Flask"
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
 
-@app.route('/admin')
-def hello_admin():
-   return 'Hello Admin'
-
-@app.route('/guest/<guest>')
-def hello_guest(guest):
-   return 'Hello %s as Guest' % guest
-
-@app.route('/user/<name>')
-def hello_user(name):
-   if name =='admin':
-      return redirect(url_for('hello_admin'))
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
    else:
-      return redirect(url_for('hello_guest', guest = name))
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
 
 if __name__ == '__main__':
    app.run(debug = True)
