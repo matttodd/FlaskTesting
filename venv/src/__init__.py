5a5b261c6f9acb813eaea27291c7e40c9117a789
matttodd <matttodd6@gmail.com>
2018-12-29 02:12:23 -0500

    i did a thing just one tho

--- venv/src/__init__.py
+++ venv/src/__init__.py
@@ -1,18 +1,14 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
+@app.route('/')
+def index():
+   return '<html><body><h1>Hello World</h1></body></html>'
 
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+@app.route('/result')
+def result():
+   dict = {'phy':50,'che':60,'maths':70}
+   return render_template('result.html', result = dict)
 
 if __name__ == '__main__':
    app.run(debug = True)
