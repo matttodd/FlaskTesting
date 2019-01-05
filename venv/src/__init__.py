60853fc47de9715dea16c5da1c2d5b7e190dcac5
matttodd <matttodd6@gmail.com>
2019-01-05 18:00:12 -0500

    back at it again with the white vans

--- venv/src/__init__.py
+++ venv/src/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-   return '<html><body><h1>Hello World</h1></body></html>'
+    return '<html><body><h1>Hello World</h1></body></html>'
+
 
 @app.route('/result')
 def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+    dict = {'phy': 50, 'che': 60, 'maths': 70}
+    return render_template('result.html', result=dict)
+
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
