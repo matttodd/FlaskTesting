cfe7a5d0a2ea49812d0daf3037d19e1139322d90
matttodd <matttodd6@gmail.com>
2019-01-06 17:26:46 -0500

    cookie MONSTER

--- venv/src/__init__.py
+++ venv/src/__init__.py
@@ -1,17 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def student():
-   return render_template('student.html')
+    return render_template('student.html')
 
-@app.route('/result', methods = ['POST', 'GET'])
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/setcookie', methods=['POST', 'GET'])
+def setcookie():
+    if request.method == 'POST':
+        user = request.form['nm']
+
+    resp = make_response(render_template('setcookie.html'))
+    resp.set_cookie('userID', user)
+
+    return resp
+
+
+@app.route('/getcookie')
+def getcookie():
+    name = request.cookies.get('userID')
+    return '<h1>welcome ' + name + '</h1>'
+
+
+@app.route('/result', methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
         result = request.form
-        return render_template('result.html', result = result)
+        return render_template('result.html', result=result)
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
